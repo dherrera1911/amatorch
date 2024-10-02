a380e901f2c43a024f152bb6f33f6e42cc8cb6cc
dherresp-adm <dherrera1911@gmail.com>
2024-10-02 16:14:22 -0400

    Divide normalized channels by sqrt(n_channels), to have unit norm

--- src/amatorch/normalization.py
+++ src/amatorch/normalization.py
@@ -1,9 +1,11 @@
 import torch
 
 def unit_norm(stimuli, c50=0):
-    """
-    Divide each channel of the stimuli by its offsetted norm
-    (i.e. || sum of squares + c50 ||).
+    """Normalize stimuli to have norm less or equal to 1.
+
+    Each channel is normalized by its offsetted norm
+    (i.e. || sum of squares + c50 ||) and by the square
+    root of the number of channels.
     ----------------
     Arguments:
     ----------------
@@ -13,8 +15,8 @@ def unit_norm(stimuli, c50=0):
     ----------------
       - stimuli_normalized: Normalized stimuli. (nStim x nDim)
     """
-    # Normalizing factors
-    offset_norm = torch.sqrt(torch.sum(stimuli**2, dim=-1) + c50)
-    # Normalize
-    return stimuli / offset_norm.unsqueeze(-1)
+    # Normalizing factor
+    n_channels = torch.as_tensor(stimuli.shape[1], dtype=stimuli.dtype, device=stimuli.device)
+    normalizing_factor = torch.sqrt(torch.sum(stimuli**2, dim=-1) + c50) * torch.sqrt(n_channels)
+    return stimuli / normalizing_factor.unsqueeze(-1)
 
