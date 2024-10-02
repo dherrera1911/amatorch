b424c3ef187162c1c7c8e8a328ed1b6fd787e87f
dherresp-adm <dherrera1911@gmail.com>
2024-10-02 14:06:23 -0400

    Add draft of amagauss inference test

--- tests/inference_test.py
+++ tests/inference_test.py
@@ -1,50 +1,62 @@
-##################
-
-# TESTS THAT THE AMA CLASS INITIALIZES AND TRAINS
-# FOR THE RELEASE BEFORE REFACTORING
-#
-##################
-
 import pytest
 import torch
 import amatorch.ama_class as cl
-import amatorch.optim as optim
-from amatorch.data import disparity_data
-import amatorch.utilities as au
+from amatorch.data import disparity_data, disparity_filters
 
-def test_inference():
+def test_ama_gauss_responses():
+    """Test that AMA-Gauss can obtain responses to stimuli
+    and that the responses are the correct shape."""
+    data = disparity_data()
+    filters = disparity_filters()
 
-    # Load the data
-    data_dict = disparity_data()
-    stimuli = data_dict['stimuli']
-    labels = data_dict['labels']
-    values = data_dict['values']
-    n_classes = len(values)
-    n_channels = stimuli.shape[-2]
-    n_dim = stimuli.shape[-1]
+    n_filters = filters.shape[0]
+    n_stimuli, n_channels, n_dim = data['stimuli'].shape
 
     ama = cl.AMAGauss(
-      stimuli=stimuli,
-      labels=labels,
+      stimuli=data['stimuli'],
+      labels=data['labels'],
       n_filters=2,
-      values=values,
     )
 
     # Assert that the filters are initialized with the correct shape
     assert ama.filters.shape == (2, n_channels, n_dim), 'Filters are not the correct shape'
-    # Assert that stimulus statistics are initialized with the correct shape
-    assert ama.stimulus_statistics.means.shape == (n_classes, n_channels * n_dim), 'Means are not the correct shape'
-    assert ama.stimulus_statistics.covariances.shape == (n_classes, n_channels * n_dim, n_channels * n_dim), 'Covariances are not the correct shape'
-    # Assert that response statistics are initialized with the correct shape
-    assert ama.response_statistics.means.shape == (n_classes, 2), 'Means are not the correct shape'
-    assert ama.response_statistics.covariances.shape == (n_classes, 2, 2), 'Covariances are not the correct shape'
 
+    # Assign pretrained filters
+    ama.filters = filters
     # Get responses to the stimuli
-    responses = ama.responses(stimuli)
+    responses = ama.responses(data['stimuli'])
 
     # Assert that the responses are the correct shape
-    assert responses.shape == (len(stimuli), 2), 'Responses are not the correct shape'
-    # Responses are not nan
+    assert responses.shape == (n_stimuli, n_filters), 'Responses are not the correct shape'
     assert not torch.isnan(responses).any(), 'Responses are nan'
+    assert not (responses.norm(dim=-1) > n_filters).any(), 'Responses are too large'
 
+
+def test_ama_gauss_inference():
+    """Test that AMA-Gauss can obtain responses to stimuli
+    and that the responses are the correct shape."""
+    data = disparity_data()
+    filters = disparity_filters()
+
+    n_filters = filters.shape[0]
+    n_stimuli, n_channels, n_dim = data['stimuli'].shape
+
+    ama = cl.AMAGauss(
+      stimuli=data['stimuli'],
+      labels=data['labels'],
+      n_filters=2,
+    )
+
+    # Assert that the filters are initialized with the correct shape
+    assert ama.filters.shape == (2, n_channels, n_dim), 'Filters are not the correct shape'
+
+    # Assign pretrained filters
+    ama.filters = filters
+    # Get responses to the stimuli
+    responses = ama.responses(data['stimuli'])
+
+    # Assert that the responses are the correct shape
+    assert responses.shape == (n_stimuli, n_filters), 'Responses are not the correct shape'
+    assert not torch.isnan(responses).any(), 'Responses are nan'
+    assert not (responses.norm(dim=-1) > n_filters).any(), 'Responses are too large'
 
