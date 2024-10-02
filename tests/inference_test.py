54c0d030f56400f56b78516811cc1133ca1ce8aa
dherresp-adm <dherrera1911@gmail.com>
2024-10-02 18:27:44 -0400

    .

--- tests/inference_test.py
+++ tests/inference_test.py
@@ -1,62 +1,97 @@
 import pytest
 import torch
+import numpy as np
 import amatorch.ama_class as cl
 from amatorch.data import disparity_data, disparity_filters
 
-def test_ama_gauss_responses():
+@pytest.fixture(scope='module')
+def data():
+    return disparity_data()
+
+@pytest.fixture(scope='module')
+def filters():
+    return disparity_filters()
+
+@pytest.fixture(scope='module')
+def responses_ref():
+    return torch.as_tensor(
+        np.loadtxt('./testing_data/dsp_responses.csv', delimiter=','),
+        dtype=torch.float32
+    )
+
+@pytest.fixture(scope='module')
+def log_likelihoods_ref():
+    return torch.as_tensor(
+        np.loadtxt('./testing_data/dsp_log_likelihoods.csv', delimiter=','),
+        dtype=torch.float32
+    )
+
+@pytest.fixture(scope='module')
+def posteriors_ref():
+    return torch.tensor(
+        np.loadtxt('./testing_data/dsp_posteriors.csv', delimiter=','),
+        dtype=torch.float32
+    )
+
+@pytest.fixture(scope='module')
+def estimates_ref():
+    return torch.tensor(
+        np.loadtxt('./testing_data/dsp_estimated_class.csv', delimiter=','),
+        dtype=int
+    )
+
+def test_ama_gauss_responses(data, filters, responses_ref):
     """Test that AMA-Gauss can obtain responses to stimuli
     and that the responses are the correct shape."""
-    data = disparity_data()
-    filters = disparity_filters()
-
     n_filters = filters.shape[0]
     n_stimuli, n_channels, n_dim = data['stimuli'].shape
 
     ama = cl.AMAGauss(
-      stimuli=data['stimuli'],
-      labels=data['labels'],
-      n_filters=2,
+        stimuli=data['stimuli'],
+        labels=data['labels'],
+        n_filters=2,
     )
-
-    # Assert that the filters are initialized with the correct shape
-    assert ama.filters.shape == (2, n_channels, n_dim), 'Filters are not the correct shape'
 
     # Assign pretrained filters
     ama.filters = filters
     # Get responses to the stimuli
     responses = ama.responses(data['stimuli'])
 
-    # Assert that the responses are the correct shape
     assert responses.shape == (n_stimuli, n_filters), 'Responses are not the correct shape'
     assert not torch.isnan(responses).any(), 'Responses are nan'
-    assert not (responses.norm(dim=-1) > n_filters).any(), 'Responses are too large'
+    assert torch.allclose(responses, responses_ref, atol=1e-6), 'Responses are not close to reference'
 
-
-def test_ama_gauss_inference():
-    """Test that AMA-Gauss can obtain responses to stimuli
-    and that the responses are the correct shape."""
-    data = disparity_data()
-    filters = disparity_filters()
-
+def test_ama_gauss_inference(data, filters, log_likelihoods_ref, posteriors_ref, estimates_ref):
+    """Test that AMA-Gauss can perform inference on stimuli
+    and that the outputs are close to the reference values."""
     n_filters = filters.shape[0]
     n_stimuli, n_channels, n_dim = data['stimuli'].shape
 
     ama = cl.AMAGauss(
-      stimuli=data['stimuli'],
-      labels=data['labels'],
-      n_filters=2,
+        stimuli=data['stimuli'],
+        labels=data['labels'],
+        n_filters=2,
     )
-
-    # Assert that the filters are initialized with the correct shape
-    assert ama.filters.shape == (2, n_channels, n_dim), 'Filters are not the correct shape'
 
     # Assign pretrained filters
     ama.filters = filters
-    # Get responses to the stimuli
-    responses = ama.responses(data['stimuli'])
+    ama.update_response_distributions()
 
-    # Assert that the responses are the correct shape
-    assert responses.shape == (n_stimuli, n_filters), 'Responses are not the correct shape'
-    assert not torch.isnan(responses).any(), 'Responses are nan'
-    assert not (responses.norm(dim=-1) > n_filters).any(), 'Responses are too large'
+    # Get inference results
+    with torch.no_grad():
+        log_likelihoods = ama.log_likelihoods(data['stimuli'])
+        posteriors = ama.posteriors(data['stimuli'])
+        estimates = ama.estimates(data['stimuli'])
 
+    assert not torch.isnan(log_likelihoods).any(), 'Log likelihoods are nan'
+    assert not torch.isnan(posteriors).any(), 'Posteriors are nan'
+
+    # Get relative norm of difference between reference and obtained values
+    log_likelihoods_diff = torch.norm(log_likelihoods - log_likelihoods_ref, dim=-1) \
+        / torch.norm(log_likelihoods_ref, dim=-1)
+    posteriors_diff = torch.norm(posteriors - posteriors_ref, dim=-1) \
+        / torch.norm(posteriors_ref, dim=-1)
+
+    assert torch.allclose(log_likelihoods_diff, torch.zeros_like(log_likelihoods_diff), atol=1e-2), 'Log likelihoods are not close to reference'
+    assert torch.allclose(posteriors_diff, torch.zeros_like(posteriors_diff), atol=1e-2), 'Posteriors are not close to reference'
+    assert torch.sum(estimates != estimates_ref) < 15, 'Estimates are not close to reference'
