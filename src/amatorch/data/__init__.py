068e088f0052b95ba19925df6b1601671d62f03d
dherresp-adm <dherrera1911@gmail.com>
2024-10-05 19:23:05 -0400

    Ruff formatting

--- src/amatorch/data/__init__.py
+++ src/amatorch/data/__init__.py
@@ -1,10 +1,13 @@
 from importlib import resources
+
 from .load import load_data, load_filters
 
-__all__ = ['disparity_data', 'disparity_filters']
+__all__ = ["disparity_data", "disparity_filters"]
+
 
 def __dir__():
-      return __all__
+    return __all__
+
 
 # Reference the resources inside the `data` directory of the package
 FILES = resources.files(__name__)
@@ -14,24 +17,32 @@ def disparity_data():
     """
     Load data from the 'disparity' subdirectory.
 
-    Returns:
-    - dict: A dictionary containing the loaded stimuli, labels, and values.
+    Returns
+    -------
+    dict
+        A dictionary containing the loaded stimuli, labels, and values.
+        The 'stimuli' are reshaped to (n_samples, n_channels, n_pixels),
+        where `n_channels=2` and `n_pixels=26`.
     """
-    data_dir = FILES / 'disparity'
+    data_dir = FILES / "disparity"
     data = load_data(data_dir=data_dir)
     n_channels = 2
     n_pixels = 26
-    data['stimuli'] = data['stimuli'].reshape(-1, n_channels, n_pixels)
+    data["stimuli"] = data["stimuli"].reshape(-1, n_channels, n_pixels)
     return data
+
 
 def disparity_filters():
     """
     Load filters from the 'disparity' subdirectory.
 
-    Returns:
-    - filters: A tensor of shape (n_filters, n_channels, n_pixels) containing the filters.
+    Returns
+    -------
+    torch.Tensor
+        A tensor of shape (n_filters, n_channels, n_pixels) containing the filters,
+        where `n_channels=2` and `n_pixels=26`.
     """
-    data_dir = FILES / 'disparity'
+    data_dir = FILES / "disparity"
     filters = load_filters(data_dir=data_dir)
     n_channels = 2
     n_pixels = 26
