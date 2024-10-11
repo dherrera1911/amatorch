f357c4dd8b56d967797efa3e873558e5d142db7c
dherresp-adm <dherrera1911@gmail.com>
2024-10-11 16:32:09 -0400

    Add data subsampling function

--- src/amatorch/data_wrangle.py
+++ src/amatorch/data_wrangle.py
@@ -1,6 +1,7 @@
 import torch
+import numpy as np
 
-__all__ = ["statistics_dim_subset"]
+__all__ = ["statistics_dim_subset", "subsample_class_points"]
 
 
 def __dir__():
@@ -31,3 +32,40 @@ def statistics_dim_subset(means, covariances, keep_inds):
     means_subset = means[:, keep_inds]
     covariances_subset = covariances[:, keep_inds][:, :, keep_inds]
     return means_subset, covariances_subset
+
+
+def subsample_class_points(points, labels, n_per_class):
+    """
+    Return a subsample of points for each class (whichever is
+    smaller of n_per_class or the total points in the class).
+
+    Parameters
+    ----------
+    points : torch.Tensor
+        Points to subsample. (n_points, n_dim)
+    labels : torch.Tensor
+        Labels or values of the points. (n_points,)
+    n_per_class : int
+        Number of points to subsample for each class.
+
+    Returns
+    -------
+    subsampled_points : torch.Tensor
+        Subsampled points. (n_classes * n_points_per_class, n_dim)
+    subsampled_labels : torch.Tensor
+        Labels or values of the subsampled points. (n_classes * n_points_per_class,)
+    """
+    n_classes = np.unique(labels)
+    subsampled_points = []
+    subsampled_labels = []
+
+    for i, label in enumerate(labels.unique()):
+        class_points = points[labels == label]
+
+        n_points_class = np.min([n_per_class, class_points.shape[0]])
+        subsampled_points.append(class_points[:n_points_class])
+        subsampled_labels.append(label.repeat(n_points_class))
+
+    subsampled_points = torch.cat(subsampled_points, dim=0)
+    subsampled_labels = torch.cat(subsampled_labels, dim=0)
+    return subsampled_points, subsampled_labels
